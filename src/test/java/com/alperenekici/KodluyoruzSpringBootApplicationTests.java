043ece75e9b3e16ec366381c0fdc5c656cf1caa7
Alperen <alperenekici2001@gmail.com>
2024-02-28 12:46:54 +0300

    first commit

---
+++ src/test/java/com/alperenekici/KodluyoruzSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperenekici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
